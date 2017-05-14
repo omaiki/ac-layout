5f059b346a6752245973a4ba81f8ce23afe39140
Oshoke Maiki <oshokemaiki@gmail.com>
2017-05-13 17:44:55 -0700

    Add jquery for carousel images, r, refactor, reformat image files, all working

---
+++ cycle.js
@@ -0,0 +1,39 @@
+
+$(document).ready(function(){
+
+
+  function changeBackgroundImage(img){
+  $('.main-promo .promo-background').clone(true).css("background-image", 'url("' + img + '")').appendTo('.main-promo');
+  $('.main-promo .promo-background').first().css('z-index', '20').fadeOut(1000, function(){
+    $(this).remove();
+    });
+  };
+
+  function cycleImage() {
+  //Check to make sure the first item exists, otherwise select the first item
+  if( $('.promo-list .active').next().length > 0)
+  {
+    $('.promo-list .active').next().click();
+  }
+  else
+  {
+    $('.promo-list li').first().click();
+  }
+  };
+
+
+  $('.promo-list li').click(function(){
+    // clearTimeout(timer);
+    $('.promo-list .active').removeClass('active');
+    $(this).addClass('active');
+    changeBackgroundImage($(this).data('image'));
+
+    setTimeout(cycleImage, 5000);
+  });
+
+    // var timer = setTimeout(cycleImage, 5000);
+
+});
+
+
+
